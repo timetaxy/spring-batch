ba913d64fb9e313a83ea5397a8bfcdd378f2ec56
prodo-developer <wjdals300@gmail.com>
2021-12-03 23:26:19 +0900

    feat : BATCH_JOB_EXECUTION 케이스 만들기 FAILED하면 성공 할때까지 계속 시도가능 COMPLETED되면 더 이상 생성 안됨

--- src/main/java/com/example/springbatch/parameterTest/JobParameterConfiguration.java
+++ src/main/java/com/example/springbatch/parameterTest/JobParameterConfiguration.java
@@ -1,72 +1,72 @@
-package com.example.springbatch.parameterTest;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.Map;
-
-/**
- * JobParameter 실습
- */
-@Configuration
-@RequiredArgsConstructor
-public class JobParameterConfiguration {
-    private final JobBuilderFactory jobBuilderFactory;
-    private final StepBuilderFactory stepBuilderFactory;
-
-    @Bean
-    public Job job() {
-        return jobBuilderFactory.get("job")
-                .start(step1())
-                .next(step2())
-                .build();
-    }
-
-    @Bean
-    public Step step1() {
-        return stepBuilderFactory.get("step1")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-
-                        // JobParameters 방식
-                        JobParameters jobParameters = contribution.getStepExecution().getJobExecution().getJobParameters();
-                        jobParameters.getString("name");
-                        jobParameters.getLong("seq");
-                        jobParameters.getDate("date");
-                        jobParameters.getDouble("age");
-
-//                        chunkContext.getStepContext().getStepExecution().getJobParameters(); 상속받고 있어서 getStepExecution() 생략
-                        // 청크는 Map으로 반환, 값만 확인 가능.
-                        Map<String, Object> jobParameters1 = chunkContext.getStepContext().getJobParameters();
-
-                        System.out.println("step1 was executed");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("step2 was executed");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
-                .build();
-    }
-}
+//package com.example.springbatch.parameterTest;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.JobParameters;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.StepContribution;
+//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.core.scope.context.ChunkContext;
+//import org.springframework.batch.core.step.tasklet.Tasklet;
+//import org.springframework.batch.repeat.RepeatStatus;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.Map;
+//
+///**
+// * JobParameter 실습
+// */
+//@Configuration
+//@RequiredArgsConstructor
+//public class JobParameterConfiguration {
+//    private final JobBuilderFactory jobBuilderFactory;
+//    private final StepBuilderFactory stepBuilderFactory;
+//
+//    @Bean
+//    public Job job() {
+//        return jobBuilderFactory.get("job")
+//                .start(step1())
+//                .next(step2())
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step1() {
+//        return stepBuilderFactory.get("step1")
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//
+//                        // JobParameters 방식
+//                        JobParameters jobParameters = contribution.getStepExecution().getJobExecution().getJobParameters();
+//                        jobParameters.getString("name");
+//                        jobParameters.getLong("seq");
+//                        jobParameters.getDate("date");
+//                        jobParameters.getDouble("age");
+//
+////                        chunkContext.getStepContext().getStepExecution().getJobParameters(); 상속받고 있어서 getStepExecution() 생략
+//                        // 청크는 Map으로 반환, 값만 확인 가능.
+//                        Map<String, Object> jobParameters1 = chunkContext.getStepContext().getJobParameters();
+//
+//                        System.out.println("step1 was executed");
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                })
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step2() {
+//        return stepBuilderFactory.get("step2")
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//                        System.out.println("step2 was executed");
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                })
+//                .build();
+//    }
+//}
