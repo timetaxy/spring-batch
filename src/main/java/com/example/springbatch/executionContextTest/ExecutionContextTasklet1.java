848c58430a51483b1a01009140508125aff26a58
prodo-developer <wjdals300@gmail.com>
2021-12-09 00:48:03 +0900

    feat : ExecutionContext를 이용한 실습

---
+++ src/main/java/com/example/springbatch/executionContextTest/ExecutionContextTasklet1.java
@@ -0,0 +1,39 @@
+package com.example.springbatch.executionContextTest;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExecutionContextTasklet1 implements Tasklet {
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        System.out.println("step1 was executed");
+
+        ExecutionContext jobExecutionContext = contribution.getStepExecution().getJobExecution().getExecutionContext();
+        ExecutionContext stepExecutionContext = contribution.getStepExecution().getExecutionContext();
+
+        String jobName = chunkContext.getStepContext().getStepExecution().getJobExecution().getJobInstance().getJobName();
+        // 검증
+        String jobNameTest = chunkContext.getStepContext().getJobName();
+        String stepName = chunkContext.getStepContext().getStepExecution().getStepName();
+
+        if(jobExecutionContext.get("jobName") == null) {
+            jobExecutionContext.put("jobName", jobName);
+        }
+
+        if(stepExecutionContext.get("stepName") == null) {
+            stepExecutionContext.put("stepName", stepName);
+        }
+
+        System.out.println("jobName : " + jobExecutionContext.get("jobName"));
+        System.out.println("stepName : " + stepExecutionContext.get("stepName"));
+
+        return RepeatStatus.FINISHED;
+    }
+
+}
--- src/main/java/com/example/springbatch/stepTest/CustomTasklet.java
+++ src/main/java/com/example/springbatch/stepTest/CustomTasklet.java
@@ -1,16 +1,16 @@
-package com.example.springbatch.stepTest;
-
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
-
-public class CustomTasklet implements Tasklet {
-    @Override
-    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-
-        System.out.println("step1 was executed Start");
-
-        return RepeatStatus.FINISHED;
-    }
-}
+//package com.example.springbatch.stepTest;
+//
+//import org.springframework.batch.core.StepContribution;
+//import org.springframework.batch.core.scope.context.ChunkContext;
+//import org.springframework.batch.core.step.tasklet.Tasklet;
+//import org.springframework.batch.repeat.RepeatStatus;
+//
+//public class CustomTasklet implements Tasklet {
+//    @Override
+//    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+//
+//        System.out.println("step1 was executed Start");
+//
+//        return RepeatStatus.FINISHED;
+//    }
+//}
