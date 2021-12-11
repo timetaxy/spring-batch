8b287db458149f6331f5dbcd7ebd4554317a9738
prodo-developer <wjdals300@gmail.com>
2021-12-11 18:44:26 +0900

    feat : JobLauncher 실습

---
+++ src/main/java/com/example/springbatch/jobLauncherTest/Member.java
@@ -0,0 +1,8 @@
+package com.example.springbatch.jobLauncherTest;
+
+import lombok.Data;
+
+@Data
+public class Member {
+    private String id;
+}
---
+++ src/main/java/com/example/springbatch/jobLauncherTest/jobLauncherController.java
@@ -0,0 +1,52 @@
+package com.example.springbatch.jobLauncherTest;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.batch.BasicBatchConfigurer;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class jobLauncherController {
+
+    @Autowired
+    private Job job;
+
+    @Autowired
+    private JobLauncher simpleJobLauncher;
+
+    @Autowired
+    private BasicBatchConfigurer basicBatchConfigurer;
+
+    @PostMapping("/batch")
+    public String launch(@RequestBody Member member) throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("id", member.getId())
+                .addDate("date", new Date())
+                .toJobParameters();
+
+        // 비동기 방식 (스프링배치가 초기화 되면서 작동 함)
+//      SimpleJobLauncher jobLauncher = (SimpleJobLauncher) basicBatchConfigurer.getJobLauncher();
+        // 동기 방식
+//      Throwable targetException = var6.getTargetException(); 아래와 같이 돌리면 타입 캐스팅이 발생합니다.
+        SimpleJobLauncher jobLauncher = (SimpleJobLauncher)simpleJobLauncher;
+        jobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        jobLauncher.run(job, jobParameters);
+
+        return "batch completed";
+    }
+
+}
