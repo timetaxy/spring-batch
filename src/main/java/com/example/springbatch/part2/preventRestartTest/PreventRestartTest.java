5309b23e8dfdbf33b60752eecad858019dac2bd8
prodo-developer <wjdals300@gmail.com>
2021-12-25 23:58:53 +0900

    feat : Job - PreventRestart 실습

---
+++ src/main/java/com/example/springbatch/part2/preventRestartTest/PreventRestartTest.java
@@ -0,0 +1,71 @@
+package com.example.springbatch.part2.preventRestartTest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Validator실습
+ * 아래와 같이 벨리데이터를 생성하지 않으면 simpleJob의 상속관계인 AbstractJob 아래의 클래스가 생성됩니다.
+ * private JobParametersValidator jobParametersValidator = new DefaultJobParametersValidator();
+ *
+ * 1. JobBulderHelper에 가장 먼저 저장 후 벨리데이터에 전달
+ * 2. AbstractJob로 이동
+ * 3. SimpleJobLauncher에 접근하여 벨리데이트 검사 ----- 제일 중요!!!
+ * 3-1. job.getJobParametersValidator().validate(jobParameters);
+ * 3-2. jobExecution = jobRepository.createJobExecution(job.getName(), jobParameters);
+ *
+ * date누락시 아래의 에러를 발견할 수 있음 (DefaultJobParametersValidator) (88라인)
+ * The JobParameters do not contain required keys: [date]
+ *
+ * 최종 : BATCH_JOB_EXECUTION_PARAMS 에 적재된 파라미터 값을 확인할 수 있다.
+ */
+@Configuration
+@RequiredArgsConstructor
+public class PreventRestartTest {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob() {
+        return jobBuilderFactory.get("batchJob")
+                .start(step1())
+                .next(step2())
+                .preventRestart()   // preventRestart로 선언하면 default : false, CommonJobProperties에서 지정
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step1 was executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        throw new RuntimeException("step was failed");
+//                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+}
