ffb54cec9ddc7461ae7a5017f2dff81040a0c3f8
prodo-developer <wjdals300@gmail.com>
2021-12-30 00:45:09 +0900

    feat : tasklet (CustomTasklet) 수동 생성 및 실습

---
+++ src/main/java/com/example/springbatch/part3/stepTest/CustomTasklet.java
@@ -0,0 +1,21 @@
+package com.example.springbatch.part3.stepTest;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+public class CustomTasklet implements Tasklet {
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+
+        String stepName = contribution.getStepExecution().getStepName();
+        String jobName = chunkContext.getStepContext().getJobName();
+
+        System.out.println("stepName = " + stepName);
+        System.out.println("jobName = " + jobName);
+
+        return RepeatStatus.FINISHED; // 한번 하고 마치기
+    }
+}
