752bc57d30b9900d54dff2f1962d5a072713694d
prodo-developer <wjdals300@gmail.com>
2022-01-09 23:19:15 +0900

    refactor : ExitStatus 부연 설명 추가

--- src/main/java/com/example/springbatch/part4/CustomExitStatusConfiguration.java
+++ src/main/java/com/example/springbatch/part4/CustomExitStatusConfiguration.java
@@ -6,10 +6,15 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * ExitStatus
+ * ExitStatus 에 존재하지 않는 exitCode 를 새롭게 정의해서 설정
+ * StepExecutionListener 의 afterStep() 메서드에서 Custom exitCode 생성 후 새로운 ExitStatus 반환
+ * Step 실행 후 완료 시점에서 현재 exitCode 를 사용자 정의 exitCode 로 수정할 수 있음
+ */
 @Configuration
 @RequiredArgsConstructor
 public class CustomExitStatusConfiguration {
--- src/main/java/com/example/springbatch/part4/PassCheckingListener.java
+++ src/main/java/com/example/springbatch/part4/PassCheckingListener.java
@@ -1,25 +1,25 @@
-package com.example.springbatch.part4;
-
-import org.springframework.batch.core.ExitStatus;
-import org.springframework.batch.core.StepExecution;
-import org.springframework.batch.core.StepExecutionListener;
-
-public class PassCheckingListener implements StepExecutionListener {
-    @Override
-    public void beforeStep(StepExecution stepExecution) {
-
-    }
-
-    @Override
-    public ExitStatus afterStep(StepExecution stepExecution) {
-
-        String exitCode = stepExecution.getExitStatus().getExitCode();
-
-        // 실행코드가 FAILED가 아니면 PASS "COMPLETED"로 들어옴
-        if(!exitCode.equals(ExitStatus.FAILED.getExitCode())) {
-            return new ExitStatus("PASS");
-        }
-
-        return null;
-    }
-}
+//package com.example.springbatch.part4;
+//
+//import org.springframework.batch.core.ExitStatus;
+//import org.springframework.batch.core.StepExecution;
+//import org.springframework.batch.core.StepExecutionListener;
+//
+//public class PassCheckingListener implements StepExecutionListener {
+//    @Override
+//    public void beforeStep(StepExecution stepExecution) {
+//
+//    }
+//
+//    @Override
+//    public ExitStatus afterStep(StepExecution stepExecution) {
+//
+//        String exitCode = stepExecution.getExitStatus().getExitCode();
+//
+//        // 실행코드가 FAILED가 아니면 PASS "COMPLETED"로 들어옴
+//        if(!exitCode.equals(ExitStatus.FAILED.getExitCode())) {
+//            return new ExitStatus("PASS");
+//        }
+//
+//        return null;
+//    }
+//}
