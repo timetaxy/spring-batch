2194a41e866e076cedd9f43b5e4e292b2aac593e
prodo-developer <wjdals300@gmail.com>
2022-01-23 23:34:26 +0900

    feat: ApiRequest, Response 생성

---
+++ src/main/java/com/example/springbatch/project/batch/domain/ApiRequestVO.java
@@ -0,0 +1,13 @@
+package com.example.springbatch.project.batch.domain;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ApiRequestVO {
+
+    private long id;
+    private ProductVO productVO;
+
+}
---
+++ src/main/java/com/example/springbatch/project/batch/domain/ApiResponseVO.java
@@ -0,0 +1,13 @@
+package com.example.springbatch.project.batch.domain;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ApiResponseVO {
+
+    private int status;
+    private String msg;
+
+}
