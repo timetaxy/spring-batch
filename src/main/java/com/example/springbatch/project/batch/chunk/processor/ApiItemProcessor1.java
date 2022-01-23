5088f273ddc8201ba6d3412a9987d660e4d30430
prodo-developer <wjdals300@gmail.com>
2022-01-23 23:33:44 +0900

    feat: 멀티스레드를 위한 Process, Writer 3개 생성

---
+++ src/main/java/com/example/springbatch/project/batch/chunk/processor/ApiItemProcessor1.java
@@ -0,0 +1,16 @@
+package com.example.springbatch.project.batch.chunk.processor;
+
+import com.example.springbatch.project.batch.domain.ApiRequestVO;
+import com.example.springbatch.project.batch.domain.ProductVO;
+import org.springframework.batch.item.ItemProcessor;
+
+public class ApiItemProcessor1 implements ItemProcessor<ProductVO, ApiRequestVO> {
+
+    @Override
+    public ApiRequestVO process(ProductVO item) throws Exception {
+        return ApiRequestVO.builder()
+                .id(item.getId())
+                .productVO(item)
+                .build();
+    }
+}
---
+++ src/main/java/com/example/springbatch/project/batch/chunk/writer/ApiItemWriter3.java
@@ -0,0 +1,14 @@
+package com.example.springbatch.project.batch.chunk.writer;
+
+import com.example.springbatch.project.batch.domain.ApiRequestVO;
+import org.springframework.batch.item.ItemWriter;
+
+import java.util.List;
+
+public class ApiItemWriter3 implements ItemWriter<ApiRequestVO> {
+
+    @Override
+    public void write(List<? extends ApiRequestVO> list) throws Exception {
+
+    }
+}
