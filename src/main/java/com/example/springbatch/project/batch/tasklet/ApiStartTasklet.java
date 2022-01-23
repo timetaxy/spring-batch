0a63d2bea568851dc604cf2e78b918061baf97b7
prodo-developer <wjdals300@gmail.com>
2022-01-23 23:35:05 +0900

    feat: JobListener에 필요한 시간 Tasklet 생성

--- src/main/java/com/example/springbatch/project/batch/tasklet/ApiStartTasklet.java
+++ src/main/java/com/example/springbatch/project/batch/tasklet/ApiStartTasklet.java
@@ -4,10 +4,16 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
 
+// 리스너를 활용할 수 있지만 시작,종료를 확인하기 위해
+@Component
 public class ApiStartTasklet implements Tasklet {
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-        return null;
+
+        System.out.println(" >> ApiService is started");
+
+        return RepeatStatus.FINISHED;
     }
 }
