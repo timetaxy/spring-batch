6ad7d9db81e561930c142b6830f8fd3c928390a4
prodo-developer <wjdals300@gmail.com>
2022-01-29 00:33:28 +0900

    refactor: api Req, Res 추가

--- src/main/java/com/example/springbatch/project/batch/domain/ApiRequestVO.java
+++ src/main/java/com/example/springbatch/project/batch/domain/ApiRequestVO.java
@@ -9,5 +9,6 @@ public class ApiRequestVO {
 
     private long id;
     private ProductVO productVO;
+    private ApiResponseVO apiResponseVO;
 
 }
--- src/main/java/com/example/springbatch/project/batch/domain/ApiResponseVO.java
+++ src/main/java/com/example/springbatch/project/batch/domain/ApiResponseVO.java
@@ -1,9 +1,13 @@
 package com.example.springbatch.project.batch.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ApiResponseVO {
 
